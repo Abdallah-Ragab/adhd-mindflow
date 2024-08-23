0987530b94ed9ac80af0b7cea6cc1375507ad695
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-08-23 05:16:06 +0300

    feat: Add login endpoint /api/user/login for user authentication

---
+++ app/api/user/login/route.ts
@@ -0,0 +1,48 @@
+import { encodeToken, getPayload } from "@/app/auth/jwt";
+import { PrismaClient } from "@prisma/client";
+import { hashPassword, passwordExtension } from "@/prisma/extensions/password";
+import { NextApiRequest } from "next";
+import { hash } from "crypto";
+
+const db = new PrismaClient().$extends(passwordExtension);
+
+export async function POST(request: NextApiRequest) {
+
+    const data = await request.json()
+    if (!data) {
+        return Response.json({
+            message: 'Invalid request',
+        });
+    }
+    const email = data.email;
+    const password = data.password;
+
+    const user = await db.user.findUnique({
+        where: {
+            email: email,
+        },
+    });
+
+    if (!user) {
+        return Response.json({
+            message: 'User not found',
+        });
+    }
+
+    const match = user.verifyPassword(password);
+    if (!match) {
+        return Response.json({
+            message: 'Invalid password',
+            input: hashPassword(password),
+            password: user.password,
+        });
+    }
+    else {
+        const token = encodeToken(user);
+        return Response.json({
+            message: 'Success',
+            token,
+
+        });
+    }
+}
