ae1ed5108aab73515c27c9cab24f6e9d4f24943b
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-08-23 05:13:10 +0300

    feat: Add passwordExtension to Prisma client for password encryption

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,12 +1,11 @@
 import { PrismaClient } from "@prisma/client";
+import { passwordExtension } from "@/prisma/extensions/password";
 
-const db = new PrismaClient();
+const db = new PrismaClient().$extends(passwordExtension);
 
 export async function GET(request: Request) {
-  console.log(request);
   try {
-    const tasks = await db.task.findMany({
-    });
+    const tasks = await db.task.findMany();
     return Response.json(tasks);
   } finally {
     await db.$disconnect();
