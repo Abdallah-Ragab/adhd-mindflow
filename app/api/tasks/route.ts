2d981aa3d1209df62db769a36da9657fce29e22d
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-08-24 04:11:12 +0300

    feat: Update tasks route to authenticate requests and return user-specific tasks

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,12 +1,31 @@
 import { PrismaClient } from "@prisma/client";
 import { passwordExtension } from "@/prisma/extensions/password";
+import { AuthenticateRequest, generateAccessToken, generateRefreshToken } from "../auth/lib/jwt";
+import { NextResponse, NextRequest } from "next/server";
 
 const db = new PrismaClient().$extends(passwordExtension);
 
-export async function GET(request: Request) {
+
+export async function GET(request: NextRequest) {
   try {
-    const tasks = await db.task.findMany();
-    return Response.json(tasks);
+    const authentication = await AuthenticateRequest(request);
+    if (authentication.error) {
+      return NextResponse.json({
+        error: authentication.error
+      }, { status: 401 })
+    }
+    else {
+      const data = await db.task.findMany({
+        where: {
+          userId: authentication.userId,
+        },
+      });
+
+      return NextResponse.json({
+        data
+      }, { status: 200 });
+
+    }
   } finally {
     await db.$disconnect();
   }
