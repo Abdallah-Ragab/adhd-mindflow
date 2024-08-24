fa5a1b8109572317523a1134424b5922b9aad34b
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-08-24 03:59:37 +0300

    feat: Refactor JWT token generation and authentication

--- app/api/auth/lib/jwt.ts
+++ app/api/auth/lib/jwt.ts
@@ -1,24 +1,85 @@
-import { sign, verify } from 'jsonwebtoken';
+import { Secret, sign, verify, VerifyErrors } from 'jsonwebtoken';
 import { User } from '@prisma/client';
+import { NextRequest } from 'next/server';
 
-const encodeToken = (user: User, duration:string = "1d") => {
+const AuthErrors = {
+    InvalidToken: {
+        message: "Invalid access token provided",
+        code: "t1"
+    },
+    ExpiredToken: {
+        message: "Provided access token has expired",
+        code: "t2"
+    },
+    MissingToken: {
+        message: "No access token provided",
+        code: "t3"
+    },
+    GenericError: {
+        message: "An error occurred while authenticating the request",
+        code: "t4"
+    }
+}
+
+export const generateAccessToken = (userId: number, duration: string = '1h') => {
     return sign(
-        { id: user.id },
-        process.env.JWT_SECRET ?? 'default-secret',
-        {
-            expiresIn: duration,
+        { 'sub': userId },
+        process.env.JWT_SECRET as Secret,
+        { expiresIn: duration }
+    )
+}
+
+export const generateRefreshToken = async (user: User, request: NextRequest) => {
+    const ip = await getIP(request);
+    return sign(
+        { 'sub': user.id, 'ip': ip },
+        process.env.JWT_SECRET as Secret,
+        { expiresIn: '7d' }
+    )
+}
+
+const getAccessToken = async (request: NextRequest) => {
+    return await request.headers.get('Authorization')?.split(' ')[1] ?? await request.cookies.get('accesstoken')?.value;
+}
+
+const getIP = async (request: NextRequest) => {
+    return await request.ip || await request.headers.get('x-forwarded-for');
+}
+
+const getPayload = async (token: string) => {
+    return await verify(token, process.env.JWT_SECRET as Secret, (err, decoded) => {
+        return err ? [err, false] : [decoded, true];
+    });
+}
+
+export const AuthenticateRequest = async (request: NextRequest) => {
+    const accessToken = await getAccessToken(request);
+    const [validation, ok] = await getPayload(accessToken); 
+
+    if (ok) {
+        return {
+            userId: validation?.sub,
+            expiresAt: validation?.exp,
+            error: false
         }
-    );
+    }
+    else {
+        let authData = AuthErrors.GenericError;
+        if (validation.name === 'TokenExpiredError') {
+            authData = AuthErrors.ExpiredToken
+        }
+        else if (validation.name === 'JsonWebTokenError') {
+            if (validation.message === 'jwt must be provided') {
+                authData = AuthErrors.MissingToken
+            }
+            else {
+                authData = AuthErrors.InvalidToken
+            }
+        }
+        return {
+            userId: null,
+            expiresAt: null,
+            error: authData
+        }
+    }
 }
-
-const decodeToken = (token: string) => {
-    return verify(token, process.env.JWT_SECRET ?? 'default-secret');
-}
-
-
-const getPayload = (token: string) => {
-    const decoded = decodeToken(token) as { id: number } | null;
-    return decoded;
-}
-
-export { encodeToken, decodeToken, getPayload }
