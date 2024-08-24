e1b98e67116f12be14b2383bae0c60f9901a1a0e
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-08-24 07:21:01 +0300

    feat: add error handling for db initialization error (usually connection error)

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -6,54 +6,65 @@ import { NextRequest, NextResponse } from "next/server";
 const db = new PrismaClient().$extends(passwordExtension);
 
 export async function POST(request: NextRequest) {
-    // @ts-ignore
-    const data = await request.json()
-    const email = data?.email;
-    const password = data?.password;
+    try {
+        // @ts-ignore
+        const data = await request.json()
+        const email = data?.email;
+        const password = data?.password;
 
-    if (!email || !password) {
+        if (!email || !password) {
+            return NextResponse.json({
+                error: {
+                    message: 'Invalid request: Please provide a valid email and password',
+                }
+            }, { status: 400 });
+        }
+
+        const user = await db.user.findUnique({
+            where: {
+                email: email,
+            },
+        });
+
+        if (!user) {
+            return NextResponse.json({
+                error: {
+                    message: 'Unauthorized: Email does not exist',
+                }
+            }, { status: 401 });
+        }
+
+        const passwordsMatch = user.verifyPassword(password);
+
+        if (!passwordsMatch) {
+            return NextResponse.json({
+                error: {
+                    message: 'Unauthorized: Invalid password',
+                }
+            }, { status: 401 });
+        }
+        else {
+            const accessToken = generateAccessToken(user.id, "10s");
+            const refreshToken = await generateRefreshToken(user, request);
+            const refreshTokenExpiry = await getRefreshTokenExpiry(refreshToken) as number * 1000;
+            const response = NextResponse.json({
+                accesstoken: accessToken,
+                refreshtoken: refreshToken,
+            }, { status: 200 })
+            // TODO: Limit refresh token to /api/auth/token route
+            response.cookies.set('accesstoken', accessToken, { httpOnly: false, path: '/', sameSite: 'strict', });
+            response.cookies.set('refreshtoken', refreshToken, { httpOnly: true,  sameSite: 'strict', expires: refreshTokenExpiry as number });;
+
+            return response;
+        }
+    } catch (PrismaClientInitializationError) {
         return NextResponse.json({
             error: {
-                message: 'Invalid request: Please provide a valid email and password',
+                message: 'Server error: Unable to initialize database connection',
             }
-        }, { status: 400 });
+        }, { status: 500 });
     }
-
-    const user = await db.user.findUnique({
-        where: {
-            email: email,
-        },
-    });
-
-    if (!user) {
-        return NextResponse.json({
-            error: {
-                message: 'Unauthorized: Email does not exist',
-            }
-        }, { status: 401 });
-    }
-
-    const passwordsMatch = user.verifyPassword(password);
-
-    if (!passwordsMatch) {
-        return NextResponse.json({
-            error: {
-                message: 'Unauthorized: Invalid password',
-            }
-        }, { status: 401 });
-    }
-    else {
-        const accessToken = generateAccessToken(user.id);
-        const refreshToken = await generateRefreshToken(user, request);
-        const refreshTokenExpiry = await getRefreshTokenExpiry(refreshToken) as number * 1000;
-        const response = NextResponse.json({
-            accesstoken: accessToken,
-            refreshtoken: refreshToken,
-        }, { status: 200 })
-
-        response.cookies.set('accesstoken', accessToken, { httpOnly: false, path: '/', sameSite: 'strict', });
-        response.cookies.set('refreshtoken', refreshToken, { httpOnly: true, path: '/', sameSite: 'strict', expires: refreshTokenExpiry as number });;
-
-        return response;
+    finally {
+        await db.$disconnect();
     }
 }
