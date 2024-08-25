235b01a2f5f7905e499d25026722d095b6a40f0a
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-08-25 11:27:32 +0300

    refactor: replace errorResponse with handleError function

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,21 +1,23 @@
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import { parseServerError } from "../../lib/error";
+import { handleError, parseServerError } from "../../lib/error";
 import { passwordExtension } from "@/prisma/extensions/password";
 import { ApiError } from "next/dist/server/api-utils";
+import { existsExtension } from "@/prisma/extensions/exists";
 
-const db = new PrismaClient().$extends(passwordExtension);
-const DEBUG = (process.env.NODE_ENV ?? "") === 'development';
+const db = new PrismaClient().$extends(passwordExtension).$extends(existsExtension);
 
 // TODO: Add validation for email, password, and name
 export async function POST(request: NextRequest) {
     try {
         const body = await request.json() as { email: string, password: string, name: string };
         if (!body.email || !body.password || !body.name) {
-            throw new ApiError(400, 'Invalid request: Please provide a valid email, password and name');
+            throw new ApiError(400, 'Invalid request: Please provide a valid email, password, and name');
         }
-        else
-        {
+        else {
+            if (await db.user.exists({ email: body.email })) {
+                throw new ApiError(400, 'Email already exists');
+            }
             const user = await db.user.create({
                 data: {
                     email: body.email,
@@ -24,6 +26,7 @@ export async function POST(request: NextRequest) {
                 }
             });
             return NextResponse.json({
+                message: 'User created successfully',
                 data: {
                     user: {
                         id: user.id,
@@ -33,23 +36,12 @@ export async function POST(request: NextRequest) {
                 }
             }, { status: 201 });
         }
-    } catch (err: any) {
+    } catch (err: Error | any) {
         // TODO: rewrite error handling using ApiError 
-        if (DEBUG) {
-            console.error(`Caught ${typeof err} @${__filename}: ` + err.message);
-        }
-        if (err instanceof ApiError) {
-            return NextResponse.json({
-                error: {
-                    message: err.message,
-                }
-            }, { status: err.statusCode });
-        }
-        return NextResponse.json({
-            ...parseServerError(err)
-        }, { status: 500 });
+        return handleError(err);
     }
     finally {
         await db.$disconnect();
     }
 }
+
