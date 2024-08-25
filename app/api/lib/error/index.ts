5de6693375faa0124fcfdd396d05460eb4fbc134
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-08-25 11:22:12 +0300

    feat: add error handling by generating response based on error type

--- app/api/lib/error/index.ts
+++ app/api/lib/error/index.ts
@@ -1,5 +1,7 @@
 import { VerifyErrors } from "jsonwebtoken";
 import errorDetails from "./details"
+import { ApiError } from "next/dist/server/api-utils";
+import { NextResponse } from "next/server";
 
 const DEBUG = (process.env.NODE_ENV ?? "") === 'development';
 
@@ -38,5 +40,28 @@ export const parseServerError = (err: Error | any) => {
             message: 'Internal server error',
             ...(DEBUG ? { details: `${err.prototype?.name ? err.prototype.name + ': ' : ''}${err.message}` } : {}),
         }
+    }
+}
+
+export const errorResponse = (err: Error) => {
+    const details = `${err.constructor.name} @${err.stack?.split('\n')[1]?.trim()?.match(/\((.*):(\d+):(\d+)\)/)?.[1]}: ` + err.message
+    if (DEBUG) {
+        (err instanceof ApiError ? console.warn : console.error)(`Caught ${details}`);
+    }
+    if (err instanceof ApiError) {
+        return NextResponse.json({
+            error: {
+                message: err.message,
+                details: DEBUG ? details : undefined
+            }
+        }, { status: err.statusCode });
+    }
+    else {
+        return NextResponse.json({
+            error: {
+                message: 'Internal Server Error',
+                details: DEBUG ? details : undefined
+            }
+        }, { status: 500 });
     }
 }
