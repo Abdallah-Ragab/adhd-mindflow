3c2f1fa08ee2c210e03a7ce0da4ac8d0c5f441b0
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-08-27 10:43:21 +0300

    feat: implement new error handling system

--- app/api/lib/error/index.ts
+++ app/api/lib/error/index.ts
@@ -1,9 +1,16 @@
 import { VerifyErrors } from "jsonwebtoken";
 import errorDetails from "./details"
-import { ApiError } from "next/dist/server/api-utils";
 import { NextResponse } from "next/server";
 
 const DEBUG = (process.env.NODE_ENV ?? "") === 'development';
+
+type traceObject = {
+    formatted: string,
+    func: string,
+    file: string,
+    line: string,
+    column: string
+}
 
 /**
  * Parses a JWT error and returns the corresponding json error response.
@@ -28,43 +35,105 @@ export const parseJWTError = (error: VerifyErrors | any) => {
     }
 }
 
-/**
- * Parses a server error and returns an object with an error message.
- * If in DEBUG mode, includes error details in the response.
- * @param {Error | any} err - The error object to parse.
- * @returns An object containing the error message and optional details.
- */
-export const parseServerError = (err: Error | any) => {
-    return {
-        error: {
-            message: 'Internal server error',
-            ...(DEBUG ? { details: `${err.prototype?.name ? err.prototype.name + ': ' : ''}${err.message}` } : {}),
-        }
+export const handleApiException = (error: ApiException | Error) => {
+    if (typeof error === 'function') {
+        throw new Error(`Expected an instance of Error class but got a reference to the class instead`);
     }
+
+    const exception = error instanceof ApiException ? error : ServerException.fromError(error)
+
+    if (DEBUG) {
+        console.warn(exception.log);
+    }
+    return exception.response()
 }
 
-export const handleError = (err: Error) => {
-    const details = `${err.constructor.name} @${err.stack?.split('\n')[1]?.trim()?.match(/\((.*):(\d+):(\d+)\)/)?.[1]}: ` + err.message
-    if (DEBUG) {
-        (err instanceof ApiError ? console.warn : console.error)(`Caught ${details}`);
+export class Exception extends Error {
+    name: string = this.constructor.name;
+    trace: traceObject;
+    log: string;
+
+    static fromError(error: Error) {
+        return new this(error.name, error.message, error.stack);
     }
-    return generateErrorResponse(err, details);
+
+    constructor(name?: string, message?: string, stack?: string) {
+        super(message);
+        this.name = name ?? this.name;
+        this.stack = stack ?? this.stack;
+        this.trace = this.parseErrorStackLine(this.stack, 1);
+        this.log = `${this.name}[${this instanceof ApiException && this.code}]: ${this.message} @${this.trace.formatted}`;
+    }
+
+    parseErrorStackLine(stack?: string, index: number = 1): traceObject {
+        const expression = /at\s(\w+)\s\((.*):(\d*):(\d*)\)/
+        const match = stack?.split('\n')[index]?.trim()?.match(expression)
+        const details = {
+            func: match?.[1] ?? "",
+            file: match?.[2] ?? "",
+            line: match?.[3] ?? "",
+            column: match?.[4] ?? ""
+        }
+        return { formatted: `[${details.func}]${details.file}:${details.line}:${details.column}`, ...details };
+    }
+
+    json() {
+        return {
+            type: this.name,
+            message: this.message,
+            trace: DEBUG ? this.trace.formatted : undefined
+        }
+    }
+
 }
-const generateErrorResponse = (err: Error, details: string| null) => {
-    if (err instanceof ApiError) {
+
+export class ApiException extends Exception {
+    statusCode: number = 400;
+    message: string = 'Something went wrong with your request';
+    code: string = "GENERIC_ERROR";
+
+    response() {
+        console.log("getResponse called in ApiException")
         return NextResponse.json({
             error: {
-                message: err.message,
-                details: DEBUG ? details : undefined
+                ...this.json()
             }
-        }, { status: err.statusCode });
+        }, { status: this.statusCode });
     }
-    else {
+
+    json() {
+        return {
+            ...super.json(),
+            code: this.code
+        }
+    }
+
+}
+
+export class ServerException extends ApiException {
+    statusCode: number = 500;
+    message: string = 'Internal server error';
+    code: string = 'SERVER_ERROR';
+    internalException: Exception;
+
+    constructor(exception: Exception) {
+        super();
+        this.internalException = exception;
+    }
+
+    static fromError(error: Error) {
+        return new this(Exception.fromError(error));
+    }
+
+    response() {
         return NextResponse.json({
             error: {
-                message: 'Internal Server Error',
-                details: DEBUG ? details : undefined
+                ...this.json(),
+                cause: DEBUG ? {
+                    ...this.internalException.json()
+                } : undefined
             }
-        }, { status: 500 });
+        }, { status: this.statusCode });
     }
+
 }
