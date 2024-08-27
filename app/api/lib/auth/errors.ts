9135de8a547fc6929d0dd6db118ff442403c6cb9
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-08-27 14:53:03 +0300

    refactor: Refactor authentication error classes

--- app/api/lib/auth/errors.ts
+++ app/api/lib/auth/errors.ts
@@ -1,28 +1,28 @@
 import { ApiException } from "@/app/api/lib/error";
 
-export class GenericAuthError extends ApiException {
+export class AuthError extends ApiException {
     message: string = 'Something went wrong with your authentication';
-    code: string = 'api.auth.generic';
+    code: string = this.code + '.auth';
     statusCode: number = 401;
 }
 
 // api.auth.login.invalid
-export class InvalidLoginError extends GenericAuthError {
+export class InvalidLoginError extends AuthError {
     message: string = 'Invalid email or password';
-    code: string = 'api.auth.login.invalid';
+    code: string = this.code + '.login.invalid';
 }
 // api.auth.login.notfound
-export class UserDoesNotExistError extends GenericAuthError {
+export class UserDoesNotExistError extends AuthError {
     message: string = 'User does not exist';
-    code: string = 'api.auth.login.notfound';
+    code: string = this.code + '.login.notfound';
 }
 // api.auth.register.invalid
-export class InvalidRegisterError extends GenericAuthError {
+export class InvalidRegisterError extends AuthError {
     message: string = 'Invalid email or password';
-    code: string = 'api.auth.register.invalid';
+    code: string = this.code + '.register.invalid';
 }
 // api.auth.register.exists
-export class EmailExistsError extends GenericAuthError {
+export class EmailExistsError extends AuthError {
     message: string = 'Email already exists';
-    code: string = 'api.auth.register.exists';
+    code: string = this.code + '.register.exists';
 }
