8283d898791d417011736bd30e43cdad27dd8d0b
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-08-28 07:05:25 +0300

    refactor: Update login logic to use new error handling system

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,72 +1,58 @@
 import { PrismaClient } from "@prisma/client";
 import { passwordExtension } from "@/prisma/extensions/password";
 import { NextRequest, NextResponse } from "next/server";
-import { parseServerError } from "@/app/api/lib/error";
+import { handleApiException, ValidationException } from "@/app/api/lib/error";
 import { generateAccessToken, generateRefreshToken } from "@/app/api/lib/auth";
 import { getTokenExp } from "@/app/api/lib/jwt";
+import { schema } from "./schema";
+import { parseValidationIssues } from "../../lib/validation";
+import { LoginPasswordIncorrectError, LoginUserDoesNotExistError } from "../../lib/auth/errors";
+import { existsExtension } from "@/prisma/extensions/exists";
+import { InvalidJsonError } from "../../lib/validation/errors";
+import type { User } from "@prisma/client";
 
-const db = new PrismaClient().$extends(passwordExtension);
+const db = new PrismaClient().$extends(passwordExtension).$extends(existsExtension);
 const DEBUG = (process.env.NODE_ENV ?? "") === 'development';
 
 export async function POST(request: NextRequest) {
     try {
+        let data
+        try {
+            data = await request.json()
+        }
+        catch (SyntaxError) {
+            throw new InvalidJsonError
+        }
+        const validation = schema.safeParse(data);
+
+        if (!validation.success) {
+            throw new ValidationException(parseValidationIssues(validation));
+        }
         // @ts-ignore
-        const data = await request.json()
-        const email = data?.email;
-        const password = data?.password;
+        const user: User & { verifyPassword: Function } = await db.user.existsOrNull({ email: validation.data.email });
+        if (!user) throw new LoginUserDoesNotExistError
 
-        if (!email || !password) {
-            return NextResponse.json({
-                error: {
-                    message: 'Invalid request: Please provide a valid email and password',
-                }
-            }, { status: 400 });
-        }
+        const passwordsMatch = user.verifyPassword(validation.data.password);
 
-        const user = await db.user.findUnique({
-            where: {
-                email: email,
-            },
-        });
+        if (!passwordsMatch) throw new LoginPasswordIncorrectError
 
-        if (!user) {
-            return NextResponse.json({
-                error: {
-                    message: 'Unauthorized: Email does not exist',
-                }
-            }, { status: 401 });
-        }
+        const accessToken = generateAccessToken(user.id, "10s");
+        const refreshToken = await generateRefreshToken(user, request);
+        const refreshTokenExpiry = await getTokenExp(refreshToken) as number * 1000;
 
-        const passwordsMatch = user.verifyPassword(password);
+        const response = NextResponse.json({
+            accesstoken: accessToken,
+            refreshtoken: refreshToken,
+        }, { status: 200 })
 
-        if (!passwordsMatch) {
-            return NextResponse.json({
-                error: {
-                    message: 'Unauthorized: Invalid password',
-                }
-            }, { status: 401 });
-        }
-        else {
-            const accessToken = generateAccessToken(user.id, "10s");
-            const refreshToken = await generateRefreshToken(user, request);
-            const refreshTokenExpiry = await getTokenExp(refreshToken) as number * 1000;
-            const response = NextResponse.json({
-                accesstoken: accessToken,
-                refreshtoken: refreshToken,
-            }, { status: 200 })
-            // TODO: Limit refresh token to /api/auth/token route
-            response.cookies.set('accesstoken', accessToken, { httpOnly: false, path: '/', sameSite: 'strict', });
-            response.cookies.set('refreshtoken', refreshToken, { httpOnly: true,  sameSite: 'strict', expires: refreshTokenExpiry as number });;
+        // TODO: Limit refresh token to /api/auth/token route
+        response.cookies.set('accesstoken', accessToken, { httpOnly: false, path: '/', sameSite: 'strict', });
+        response.cookies.set('refreshtoken', refreshToken, { httpOnly: true, sameSite: 'strict', expires: refreshTokenExpiry as number });;
 
-            return response;
-        }
+        return response;
+
     } catch (err: Error | any) {
-        if (DEBUG) {
-            console.error("Caught Error: " + err.message);
-        }
-        return NextResponse.json({
-            ...parseServerError(err)
-        }, { status: 500 });
+        return handleApiException(err);
     }
     finally {
         await db.$disconnect();
