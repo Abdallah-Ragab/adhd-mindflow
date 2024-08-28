78cdef09241bad33024d37435c8c65c75b48bdca
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-08-28 04:21:08 +0300

    feat: Update authentication error classes and add new error handling for user not found and authorized token usage exception

--- app/api/lib/auth/errors.ts
+++ app/api/lib/auth/errors.ts
@@ -6,13 +6,19 @@ export class AuthError extends ApiException {
     statusCode: number = 401;
 }
 
+// api.auth.user.notfound
+export class UserNotFoundError extends AuthError {
+    message: string = 'User not found';
+    code: string = this.code + '.user.notfound';
+}
+
 // api.auth.login.invalid
 export class InvalidLoginError extends AuthError {
     message: string = 'Invalid email or password';
     code: string = this.code + '.login.invalid';
 }
 // api.auth.login.notfound
-export class UserDoesNotExistError extends AuthError {
+export class LoginUserDoesNotExistError extends AuthError {
     message: string = 'User does not exist';
     code: string = this.code + '.login.notfound';
 }
@@ -22,7 +28,13 @@ export class InvalidRegisterError extends AuthError {
     code: string = this.code + '.register.invalid';
 }
 // api.auth.register.exists
-export class EmailExistsError extends AuthError {
+export class RegisterEmailExistsError extends AuthError {
     message: string = 'Email already exists';
     code: string = this.code + '.register.exists';
+}
+
+// api.auth.token.unauthorized
+export class UnauthorizedUseOfTokenError extends AuthError {
+    message: string = 'Unauthorized use of token';
+    code: string = this.code + '.token.unauthorized';
 }
