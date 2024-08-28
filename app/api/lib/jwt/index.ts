fb97caa8f945c1d69f789724eef8ffbdc8f0dbdd
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-08-28 03:43:29 +0300

    refactor: Update JWT decoding and error handling and the implementation of dependant functions

--- app/api/lib/jwt/index.ts
+++ app/api/lib/jwt/index.ts
@@ -2,16 +2,15 @@ import { Secret, verify } from "jsonwebtoken";
 import { parseJWTError } from "../error";
 
 /**
- * Decodes a JWT token using the provided secret key.
+ * Decode a JWT token using the provided secret key.
  * @param {string} token - The JWT token to decode.
- * @returns {Promise<{ error: Error | null, payload: any }>} A promise that resolves to an object containing the error (if any) and the decoded payload.
+ * @returns {Promise<any>} A promise that resolves to the decoded token payload.
  */
-export const decodeToken = async (token: string) => {
+export const decodeToken = async (token: string) : Promise<{ sub: number, exp: number, ip?:string }> => {
+    // @ts-ignore
     return await verify(token, process.env.JWT_SECRET as Secret, (err, decoded) => {
-        return {
-            error: err ?? null,
-            payload: decoded
-        }
+        if (err) throw parseJWTError(err)
+        return decoded;
     });
 }
 
@@ -21,45 +20,35 @@ export const decodeToken = async (token: string) => {
  * @returns {Promise<number | null>} A Promise that resolves to the expiry time of the token, or null if an error occurs.
  */
 export const getTokenExp = async (token: string): Promise<number | null> => {
-    // @ts-ignore
-    const decodedToken = await decodeToken(token) as { error: any, payload };
-    if (!decodedToken.error) return decodedToken.payload?.exp;
-    else return null;
+    const decodedToken = await decodeToken(token);
+    return decodedToken?.exp;
 }
 
 /**
- * Validates the access token by decoding it and checking for errors.
+ * Validates the access token by decoding it and extracting the user ID, expiration time,
+ * and error status.
  * @param {string} accessToken - The access token to validate.
- * @returns {Promise<{ userId: number | null, expiresAt: number | null, error: Object | boolean }>} 
- * An object containing the user ID, expiration time, and error status of the access token.
+ * @returns A Promise that resolves to an object containing the user ID, expiration time,
+ * and error status.
  */
-export const validateAccessToken = async (accessToken: string): Promise<{ userId?: number, expiresAt?: number, error: Object | boolean }> => {
-    // @ts-ignore
-    const decodedToken = await decodeToken(refreshToken) as { error: any, payload };
-    if (!decodedToken.error) return {
-        userId: decodedToken.payload?.sub,
-        expiresAt: decodedToken.payload?.exp,
-        error: false
+export const validateAccessToken = async (accessToken: string): Promise<{ userId?: number, expiresAt?: number }> => {
+    const decodedToken = await decodeToken(accessToken);
+    return {
+        userId: decodedToken?.sub,
+        expiresAt: decodedToken?.exp,
     }
-
-    else return parseJWTError(decodedToken.error)
-
 }
 
 /**
  * Validates a refresh token by decoding it and extracting relevant information.
  * @param {string} refreshToken - The refresh token to validate.
- * @returns A Promise that resolves to an object containing userId, ip, expiresAt, and error status.
+ * @returns {Promise<{ userId?: number, ip?: string, expiresAt?: number }>} An object containing the decoded information from the refresh token.
  */
-export const validateRefreshToken = async (refreshToken: string): Promise<{ userId?: number, ip?: string, expiresAt?: number, error: Object | boolean }> => {
-    // @ts-ignore
-    const decodedToken = await decodeToken(refreshToken) as { error: any, payload };
-    if (!decodedToken.error) return {
-        userId: decodedToken.payload?.sub,
-        ip: decodedToken.payload?.ip,
-        expiresAt: decodedToken.payload?.exp,
-        error: false
+export const validateRefreshToken = async (refreshToken: string): Promise<{ userId?: number, ip?: string, expiresAt?: number }> => {
+    const decodedToken = await decodeToken(refreshToken);
+    return {
+        userId: decodedToken?.sub,
+        ip: decodedToken?.ip,
+        expiresAt: decodedToken?.exp,
     }
-
-    else return parseJWTError(decodedToken.error);
 }
