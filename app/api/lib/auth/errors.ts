b46d0465148adc28960457a683dfa59ae3d4692e
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-08-28 05:56:32 +0300

    feat: add LoginPasswordIncorrectError exception

--- app/api/lib/auth/errors.ts
+++ app/api/lib/auth/errors.ts
@@ -1,39 +1,74 @@
 import { ApiException } from "@/app/api/lib/error";
 
+/**
+ * error class for authentication-related errors. Extends the base ApiException class.
+ * returns The HTTP status code for authentication errors (401 - Unauthorized).
+ * @extends ApiException
+ */
 export class AuthError extends ApiException {
     message: string = 'Something went wrong with your authentication';
     code: string = this.code + '.auth';
     statusCode: number = 401;
 }
 
-// api.auth.user.notfound
+/**
+ * error class for when a user is not found during authentication.
+ * @extends AuthError
+ */
 export class UserNotFoundError extends AuthError {
     message: string = 'User not found';
     code: string = this.code + '.user.notfound';
 }
 
-// api.auth.login.invalid
-export class InvalidLoginError extends AuthError {
+/**
+ * Represents an error that occurs during the login process.
+ * @extends AuthError
+ */
+export class LoginError extends AuthError {
     message: string = 'Invalid email or password';
-    code: string = this.code + '.login.invalid';
-}
-// api.auth.login.notfound
-export class LoginUserDoesNotExistError extends AuthError {
-    message: string = 'User does not exist';
-    code: string = this.code + '.login.notfound';
-}
-// api.auth.register.invalid
-export class InvalidRegisterError extends AuthError {
-    message: string = 'Invalid email or password';
-    code: string = this.code + '.register.invalid';
-}
-// api.auth.register.exists
-export class RegisterEmailExistsError extends AuthError {
-    message: string = 'Email already exists';
-    code: string = this.code + '.register.exists';
+    code: string = this.code + '.login';
 }
 
-// api.auth.token.unauthorized
+/**
+ * Custom error class for when a login user does not exist.
+ * @extends LoginError
+ */
+export class LoginUserDoesNotExistError extends LoginError {
+    message: string = 'User does not exist';
+    code: string = this.code + '.notfound';
+}
+
+/**
+ * Represents an error that occurs when the provided password is incorrect during login.
+ * @extends LoginError
+ */
+export class LoginPasswordIncorrectError extends LoginError {
+    message: string = 'Password is incorrect';
+    code: string = this.code + '.incorrect';
+}
+
+/**
+ * Represents an error that occurs during user registration.
+ * @extends AuthError
+ */
+export class RegisterError extends AuthError {
+    message: string = 'Invalid email or password';
+    code: string = this.code + '.register';
+}
+
+/**
+ * Represents an error that occurs when attempting to register with an email that already exists.
+ * @extends RegisterError
+ */
+export class RegisterEmailExistsError extends RegisterError {
+    message: string = 'Email already exists';
+    code: string = this.code + '.exists';
+}
+
+/**
+ * Represents an error that occurs when an unauthorized use of a token is detected.
+ * @extends AuthError
+ */
 export class UnauthorizedUseOfTokenError extends AuthError {
     message: string = 'Unauthorized use of token';
     code: string = this.code + '.token.unauthorized';
