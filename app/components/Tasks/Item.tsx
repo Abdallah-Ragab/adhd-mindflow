5f2ecf23614846d7482c06b0069b104f0217ab79
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-09-06 05:18:04 +0300

    feat: added details section that shows on click

--- app/components/Tasks/Item.tsx
+++ app/components/Tasks/Item.tsx
@@ -1,11 +1,22 @@
+'use client';
+import React from "react"
 import { TimerRounded } from "@mui/icons-material"
-import { Checkbox, ListItem } from "@mui/joy"
+import { Box, Checkbox, ListItem } from "@mui/joy"
 import { formatTime } from "@/app/lib/time"
+import { SmallTimer } from "../timer";
 
-export default function TimeTaskItem({ task } : { task: { title: string, totalTime: number, spentTime:number} }) {
+export default function TimeTaskItem({ task }: { task: { title: string, totalTime: number, spentTime: number, done:boolean } }) {
+    const [open, setOpen] = React.useState(false)
+    const [checked, setChecked] = React.useState(task.done)
     return (
         <div>
-            <ListItem variant='soft' className='w-full h-12 !rounded-xl flex justify-between items-center'>
+            <ListItem variant='soft' className='w-full h-12 !rounded-xl flex justify-between items-center' onClick={() => { setOpen(open => !open) }} sx={{
+                cursor: 'pointer',
+                backgroundColor: open ? 'neutral.softActiveBg' : 'neutral.softBg',
+                '&:hover': {
+                    backgroundColor: 'neutral.softHoverBg',
+                }
+            }}>
                 <div className='flex items-center'>
                     <Checkbox className='mr-2' />
                     {task.title}
@@ -17,6 +28,14 @@ export default function TimeTaskItem({ task } : { task: { title: string, totalTi
                     <TimerRounded />
                 </div>
             </ListItem>
+            <Box className='w-full duration-300 transition-all ease-in-out overflow-hidden p-4' sx={{
+                height: open ? 'auto' : '0px',
+                opacity: open ? 1 : 0,
+                padding: open ? '' : '0px',
+            }}>
+                Details
+                <SmallTimer id={task.title} persistent={true} />
+            </Box>
         </div>
     )
 }
