082b93a7cdecabae83feb2e52bfa58c9c49171fc
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-09-06 03:19:50 +0300

    feat: Add Layout component
    Add the Layout component which includes the Main, Header, Root, RootWithMobileNav, SideDrawer, and SideNav components.

---
+++ app/components/Layout/index.ts
@@ -0,0 +1,15 @@
+import Main from "./Main";
+import Header from "./Header";
+import Root from "./Root";
+import RootWithMobileNav from "./RootWithMobileNav";
+import SideDrawer from "./SideDrawer";
+import SideNav from "./SideNav";
+
+export default {
+    Main,
+    Header,
+    Root,
+    RootWithMobileNav,
+    SideDrawer,
+    SideNav
+}
