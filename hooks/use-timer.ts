f20ef5071f727cbc0e81ba00ae0150e4a3a7dd6c
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-09-30 14:29:07 +0300

    feat: Add useTimer and useToast custom hooks

---
+++ hooks/use-timer.ts
@@ -0,0 +1,59 @@
+import { useEffect, useRef, useState } from "react"
+
+const useTimer = (id: string, persistent: Boolean = false, initialTime = 0) => {
+
+    const getStoredTimer = (id: string) => {
+        const storedTimer = localStorage.getItem(id)
+        return storedTimer ? JSON.parse(storedTimer) : null
+    }
+    const getStoredTimerValue = (id: string, key: string) => {
+        const timer = getStoredTimer(id) ?? { time: initialTime, timerOn: false, lastOn: new Date().getTime() }
+        return timer[key]
+    }
+
+    const uniqeId = `timer-${id}`
+    const [time, setTime] = useState(getStoredTimerValue(uniqeId, 'time'))
+    const [timerOn, setTimerOn] = useState(getStoredTimerValue(uniqeId, 'timerOn'))
+    const lastOn = useRef(getStoredTimerValue(uniqeId, 'lastOn'))
+
+    // on Mount
+    // If persistent, check if the timer was on when the page was last closed and update the time
+    useEffect(() => {
+        if (persistent) {
+            const now = new Date().getTime()
+            const diff = Math.floor((now - lastOn.current) / 1000)
+            if (timerOn && (diff > 1)) {
+                setTime(time + diff)
+            }
+        }
+        else {
+            setTimerOn(false)
+        }
+    }, [])
+
+    // on time or timerOn change
+    // Save the timer to local storage
+    useEffect(() => {
+        localStorage.setItem(uniqeId, JSON.stringify({
+            time,
+            timerOn: timerOn,
+            lastOn: lastOn.current
+        }))
+    }, [time, timerOn])
+
+    // on timerOn change
+    // start/remove an interval to update the time
+    useEffect(() => {
+        if (timerOn) {
+            const interval = setInterval(() => {
+                setTime((time: number) => time + 1)
+                lastOn.current = new Date().getTime()
+            }, 1000)
+            return () => clearInterval(interval)
+        }
+    }, [timerOn])
+
+    return { time, timerOn, setTimerOn }
+}
+
+export default useTimer;
